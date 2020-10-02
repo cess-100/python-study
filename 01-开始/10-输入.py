9b562bd5f138a6faefb857bdecbc942190ce88b2
cess <270017772@qq.com>
2020-10-02 16:30:34 +0800

    提交

--- 01-开始/10-输入.py
+++ 01-开始/10-输入.py
@@ -1,9 +1,9 @@
-'''
-当程序执⾏行行到input ，等待⽤用户输⼊入，输⼊入完成之后才继续向下执⾏行行。
-在Python中， input 接收⽤用户输⼊入后，⼀一般存储到变量量，⽅方便便使⽤用。
-在Python中， input 会把接收到的任意⽤用户输⼊入的数据都当做字符串串处理理。
-'''
+"""
+当程序执行到input ，等待用户输入，输入完成之后才继续向下执行。
+在Python中， input 接收用户输⼊入后，一般存储到变量，方便使用。
+在Python中， input 会把接收到的任意用户输入的数据都当做字符串处理。
+"""
 
 password = input('请输出您的密码：')
-print('您输入的密码是%s' % (password))
+print('您输入的密码是%s' % password)
 print(type(password))
