b53d2149855823adb979422e27e919098d8c6045
cess <270017772@qq.com>
2020-12-23 22:19:25 +0800

    提交

--- 03-选择/08-猜拳游戏.py
+++ 03-选择/08-猜拳游戏.py
@@ -1,7 +1,7 @@
 """
 提示：0-石头，1-剪刀，2-布
 1. 出拳
-    玩家输⼊入出拳
+    玩家输入出拳
     电脑随机出拳
 2. 判断输赢
     玩家获胜
@@ -19,7 +19,7 @@ computer = random.randint(0, 2)
 print('电脑出%d' % computer)
 
 # 2.判断输赢
-if ((player == 0 and computer == 1) or (player == 1 and computer == 2) or (player == 2 and computer == 0)):
+if (player == 0 and computer == 1) or (player == 1 and computer == 2) or (player == 2 and computer == 0):
     print('玩家获胜')
 elif player == computer:
     print('平局')
