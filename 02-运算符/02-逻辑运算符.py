c55510cb35ef0a8fdac736bb3e00d140be2509b1
cess <270017772@qq.com>
2020-12-23 21:59:52 +0800

    提交

--- 02-运算符/02-逻辑运算符.py
+++ 02-运算符/02-逻辑运算符.py
@@ -12,7 +12,7 @@ print(not False)
 print(not c > b)
 
 
-# and运算符，只要有⼀一个值为0，则结果为0，否则结果为最后⼀一个⾮非0数字
+# and运算符，只要有⼀个值为0，则结果为0，否则结果为最后⼀个非0数字
 print(a and b)  # 0
 print(b and a)  # 0
 print(a and c)  # 0
@@ -20,7 +20,7 @@ print(c and a)  # 0
 print(b and c)  # 2
 print(c and b)  # 1
 
-# or运算符，只有所有值为0结果才为0，否则结果为第⼀一个⾮非0数字
+# or运算符，只有所有值为0结果才为0，否则结果为第⼀个非0数字
 print(a or b)  # 1
 print(a or c)  # 2
 print(b or c)  # 1
